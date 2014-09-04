c3afd7687f38a7c019deba534965b18b71f1f433
Javon Harper <javon.d.harper@gmail.com>
2014-09-03 22:03:56 -0400

    Add edit and delete buttons to action bar instead of buttons

--- app/src/main/java/com/javonharper/familiar/HabitShowActivity.java
+++ app/src/main/java/com/javonharper/familiar/HabitShowActivity.java
@@ -13,6 +13,9 @@ import android.widget.Toast;
 
 public class HabitShowActivity extends Activity {
 
+    private Habit habit;
+    private HabitController controller;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,8 +24,8 @@ public class HabitShowActivity extends Activity {
         Intent intent = getIntent();
         Integer habitId = Integer.valueOf(intent.getIntExtra(HabitIndexActivity.HABIT_ID, 0));
 
-        final HabitController controller = new HabitController(this);
-        final Habit habit = controller.getHabit(habitId);
+        controller = new HabitController(this);
+        habit = controller.getHabit(habitId);
 
         setTitle(habit.getName());
         getActionBar().setDisplayHomeAsUpEnabled(true);
@@ -45,33 +48,6 @@ public class HabitShowActivity extends Activity {
         final TextView currentProgress = (TextView) findViewById(R.id.habit_current_progress);
         currentProgress.setTypeface(font);
         currentProgress.setText(habit.getCurrentProgress().toString());
-
-        Button deleteButton = (Button) findViewById(R.id.delete_habit);
-        deleteButton.setTypeface(font);
-        deleteButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                controller.deleteHabit(habit.getId());
-
-                String message = "Habit \"" + habit.getName() + "\" deleted.";
-                Toast.makeText(view.getContext(), message, Toast.LENGTH_SHORT).show();
-
-                Intent intent = new Intent(view.getContext(), HabitIndexActivity.class);
-                intent.putExtra(HabitIndexActivity.HABIT_ID, habit.getId().intValue());
-                startActivity(intent);
-            }
-        });
-
-        Button editButton = (Button) findViewById(R.id.edit_habit);
-        editButton.setTypeface(font);
-        editButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), HabitEditActivity.class);
-                intent.putExtra(HabitIndexActivity.HABIT_ID, habit.getId().intValue());
-                startActivity(intent);
-            }
-        });
 
         Button increment = (Button) findViewById(R.id.increment);
         increment.setTypeface(font);
@@ -112,12 +88,31 @@ public class HabitShowActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        Intent intent;
         switch (id) {
             case android.R.id.home:
-                Intent intent = new Intent(getApplicationContext(), HabitIndexActivity.class);
+                intent = new Intent(getApplicationContext(), HabitIndexActivity.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(intent);
                 return true;
+
+            case R.id.action_edit:
+                intent = new Intent(this, HabitEditActivity.class);
+                intent.putExtra(HabitIndexActivity.HABIT_ID, habit.getId().intValue());
+                startActivity(intent);
+                return true;
+
+            case R.id.action_delete:
+                controller.deleteHabit(habit.getId());
+
+                String message = "Habit \"" + habit.getName() + "\" deleted.";
+                Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+
+                intent = new Intent(this, HabitIndexActivity.class);
+                intent.putExtra(HabitIndexActivity.HABIT_ID, habit.getId().intValue());
+                startActivity(intent);
+                return true;
+
             case R.id.action_settings:
                 return true;
         }
